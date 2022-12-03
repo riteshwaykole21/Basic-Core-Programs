f60e269e02718020f3880a76b2c333ffd9dca2f2
“riteshwaykole21” <riteshwaykole21@gmail.com>
2022-12-03 15:39:49 +0530

    [Ritesh]add :Leap Year Program

---
+++ BasicCorepromblem/LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorepromblem
+{
+    public class LeapYear
+    { 
+        public void Leap()
+        {
+            Console.WriteLine("Enter the year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            {
+                Console.WriteLine("{0} is a Leap Year", year);
+            }  
+            else
+            {
+                Console.WriteLine("{0} is not a leap year " , year);
+            }
+            
+        }
+            
+    
+    }
+}
--- BasicCorepromblem/Program.cs
+++ BasicCorepromblem/Program.cs
@@ -10,7 +10,7 @@ namespace BasicCorepromblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Exit");
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3.Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
             while (flag)
@@ -22,8 +22,13 @@ namespace BasicCorepromblem
                         coin.Play();
                         break;
                     case 2:
+                        LeapYear check =new LeapYear();
+                        check.Leap(); 
+                        break;
+                    case 3:
                         flag = false;
                         break;
+                    
                 }
             }
         }
