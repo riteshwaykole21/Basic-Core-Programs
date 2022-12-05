a51589f7eb48fb496c4188641073ba6721014a93
“riteshwaykole21” <riteshwaykole21@gmail.com>
2022-12-05 13:46:33 +0530

    [Ritesh]add :Harmonic Numbers Checkout

---
+++ BasicCorepromblem/HarmonicNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorepromblem
+{
+    public class HarmonicNumber
+    {
+        public void Harmonic()
+        {
+            Console.WriteLine("Enter the last harmonic number");
+            double num = Convert.ToDouble(Console.ReadLine());
+            double harmonicSum = 0;
+            for (double i = 1; i <= num; i++)
+            {
+                Console.WriteLine("1/" + i + "=" + (1 / i));
+                harmonicSum += (1 / i);
+            }
+            Console.WriteLine("The Sum of harmonic number is :" + harmonicSum);
+        }
+    }
+}
--- BasicCorepromblem/Program.cs
+++ BasicCorepromblem/Program.cs
@@ -10,7 +10,7 @@ namespace BasicCorepromblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Exit");
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
             while (flag)
@@ -31,6 +31,11 @@ namespace BasicCorepromblem
                         break;
 
                     case 4:
+                        HarmonicNumber sum = new HarmonicNumber();
+                        sum.Harmonic();
+                        break;
+
+                    case 5:
                         flag = false;
                         break;
 
